31210f0cd774c46ec5c4557416fe78f390511605
Benjamin Nims <bennims93@gmail.com>
2023-01-25 10:28:20 -0600

    Maximum Subarray Problem

--- Algorithms/ContainsDuplicate.cs
+++ Algorithms/ContainsDuplicate.cs
@@ -3,9 +3,29 @@ namespace LeetCodeProblems.Algorithms
 {
 	public class EmptyClass
 	{
-		public EmptyClass()
-		{
-		}
-	}
+        // Given an integer array nums, return true if any value appears at least twice in the array,
+        // and return false if every element is distinct.
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            // Initialize a new Hashset
+            HashSet<int> set = new();
+
+            // Iterate over the array
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // If condition of problem is not met, add element to Hashset.
+                if (set.Contains(nums[i]))
+                {
+                    return true;
+                }
+
+                set.Add(nums[i]);
+            }
+
+            // If we're at this point, all elements are distinct.
+            return false;
+        }
+    }
 }
 
---
+++ Algorithms/MaximumSubarray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCodeProblems.Algorithms
+{
+	public class MaximumSubarray
+	{
+        // QUESTION: Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+		// Note that we need not return the subarray itself, only the largest sum that can be obtained
+		// from adding consecutive numbers in the array.
+
+        public static int FindMaxSubarray(int[] nums)
+		{
+			// Initialize values for storing last and current maximums for comparison.
+			int max = nums[0];
+			int currentMax = nums[0];
+
+			// For loop begins iterating at nums[1] to avoid the first operation yielding:
+			// Math.Max(nums[0], nums[0] * 2)
+			for (int i = 1; i < nums.Length; i++)
+			{
+				// this checks whether the value being iterated over is non-negative
+				currentMax = Math.Max(nums[i], currentMax + nums[i]);
+
+				// this sets the return value equal to either itself, or currentMax if it is greater after this iteration
+				// of the for loop.
+				max = Math.Max(max, currentMax);
+			}
+
+			return max;
+		}
+	}
+}
+
