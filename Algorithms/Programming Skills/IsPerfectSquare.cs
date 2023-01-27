efbcd6693f57ba61600cb2c6ffa8d5df5fd1807d
Benjamin Nims <bennims93@gmail.com>
2023-01-27 16:24:24 -0600

    IsPerfectSquare

---
+++ Algorithms/Programming Skills/IsPerfectSquare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCodeProblems.Algorithms.ProgrammingSkills
+{
+	public class IsPerfectSquare
+	{
+        public bool IsPerfectSq(int num)
+        {
+            // Get the number to the power of 1/2 (the square root) and mod
+            // 1 to see if it's an integer: a perfect square will yield mod 0.
+            return (Math.Pow(num, 0.5)) % 1 == 0;
+        }
+    }
+}
+
